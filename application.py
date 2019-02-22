d633b4640c001c5d91e80342f00c9c2935f04dfa
marhoy <martin.hoy@pvv.ntnu.no>
2019-02-22 23:39:56 +0100

    Update

--- application.py
+++ application.py
@@ -5,6 +5,9 @@ import restapi
 # AWS EB requires the application to be named appliction
 application = restapi.app
 
+# Azure requires the application to be named app
+app = restapi.app
+
 # For local debugging
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5000, debug=True)
